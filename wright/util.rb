d676d433498b422f25893692ca0161883bb02d35
Sebastian Boehm <sebastian@sometimesfood.org>
2012-05-06 01:00:19 +0200

    Use class name instead of to_s for resource names

--- wright/util.rb
+++ wright/util.rb
@@ -3,7 +3,7 @@ require 'wright/util/stolen_from_activesupport'
 module Wright
   module Util
     def self.class_to_resource_name(klass)
-      underscore(klass.to_s).split('/').last
+      underscore(klass.name).split('/').last
     end
   end
 end
