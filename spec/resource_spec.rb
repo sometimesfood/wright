4151de1dc9dbdf3c861431589a3d4eb056fb0d4b
Sebastian Boehm <sebastian@sometimesfood.org>
2012-06-30 12:41:18 +0200

    Add spec for Wright::Resource

---
+++ spec/resource_spec.rb
@@ -0,0 +1,30 @@
+$LOAD_PATH.unshift File.dirname(__FILE__)
+require 'spec_helper'
+require 'wright/resource'
+
+include Wright
+
+describe Resource do
+  before(:each) do
+    # duplicate Wright::Resource for testing
+    resource_module = Resource.dup
+    @recipe = Class.new do
+      extend resource_module
+    end
+    @resource_module = resource_module
+  end
+
+  it 'should register new resources at runtime' do
+    resource_class = Class.new do
+      def self.name; 'ResourceKlass'; end
+      def initialize(name); end
+    end
+
+    @resource_module.register(resource_class)
+
+    resource_name = Util.class_to_resource_name(resource_class)
+    @recipe.must_respond_to(resource_name)
+    resource = @recipe.send(resource_name)
+    resource.must_be_instance_of(resource_class)
+  end
+end
