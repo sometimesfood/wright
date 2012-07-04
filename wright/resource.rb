ef399775ddbb39312fd314d5da73f5f9b627c04e
Sebastian Boehm <sebastian@sometimesfood.org>
2012-07-04 13:47:45 +0200

    Remove hardcoded module name in Resource::register

--- wright/resource.rb
+++ wright/resource.rb
@@ -4,8 +4,9 @@ module Wright
   module Resource
     def self.register(resource_class)
       method_name = Util.class_to_resource_name(resource_class)
+      this_module = self
       define_method(method_name) do |name, &block|
-        Resource.yield_resource(resource_class, name, &block)
+        this_module.yield_resource(resource_class, name, &block)
       end
     end
 
