10791499f52ddfe2f0b1e7e02319dcc00c41a792
Sebastian Boehm <sebastian@sometimesfood.org>
2012-08-26 15:49:34 +0200

    Change Wright::Provider from module to class

--- lib/wright/provider.rb
+++ lib/wright/provider.rb
@@ -1,6 +1,6 @@
 require 'wright/util/recursive_autoloader'
 
-module Wright::Provider
+class Wright::Provider
   # Public: Wright standard provider directory.
   PROVIDER_DIR = File.expand_path('provider', File.dirname(__FILE__))
   Wright::Util::RecursiveAutoloader.add_autoloads(PROVIDER_DIR, self.name)
