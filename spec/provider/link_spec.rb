f80792b403eeb1c7b00f9b9a7c2f5b3552a8937d
Sebastian Boehm <sebastian@sometimesfood.org>
2012-09-12 12:38:45 +0200

    Add #updated? description to link provider spec

--- spec/provider/link_spec.rb
+++ spec/provider/link_spec.rb
@@ -14,67 +14,66 @@ describe Wright::Provider::Link do
     FakeFS::FileSystem.clear
   end
 
-  it 'should set its update status if a link was created' do
-    FakeFS do
-      link = Wright::Provider::Link.new(@link_resource)
-      link.create!
-      assert link.updated?
+  describe '#updated?' do
+    it 'should return the update status if a link was created' do
+      FakeFS do
+        link = Wright::Provider::Link.new(@link_resource)
+        link.create!
+        assert link.updated?
+      end
     end
-  end
 
-  it 'should not change its update status if no link was created' do
-    FakeFS do
-      link = Wright::Provider::Link.new(@link_resource)
-      assert !link.updated?
-      FileUtils.ln_sf(@link_resource.source, @link_resource.target)
-      link.create!
-      assert !link.updated?
+    it 'should return the update status if a link was not created' do
+      FakeFS do
+        link = Wright::Provider::Link.new(@link_resource)
+        FileUtils.ln_sf(@link_resource.source, @link_resource.target)
+        link.create!
+        assert !link.updated?
 
-      FileUtils.rm(@link_resource.target)
-      FileUtils.touch(@link_resource.target)
-      proc { link.create! }.must_raise Errno::EEXIST
-      assert !link.updated?
+        FileUtils.rm(@link_resource.target)
+        FileUtils.touch(@link_resource.target)
+        proc { link.create! }.must_raise Errno::EEXIST
+        assert !link.updated?
+      end
     end
-  end
 
-  it 'should set its update status if a link was changed' do
-    FakeFS do
-      FileUtils.ln_sf('old-source', @link_resource.target)
-      link = Wright::Provider::Link.new(@link_resource)
-      link.create!
-      assert link.updated?
+    it 'should return the update status if a link was changed' do
+      FakeFS do
+        FileUtils.ln_sf('old-source', @link_resource.target)
+        link = Wright::Provider::Link.new(@link_resource)
+        link.create!
+        assert link.updated?
+      end
     end
-  end
 
-  it 'should not change its update status if a link was not changed' do
-    FakeFS do
-      link = Wright::Provider::Link.new(@link_resource)
-      assert !link.updated?
-      FileUtils.ln_sf(@link_resource.source, @link_resource.target)
-      link.create!
-      assert !link.updated?
+    it 'should return the update status if a link was not changed' do
+      FakeFS do
+        link = Wright::Provider::Link.new(@link_resource)
+        FileUtils.ln_sf(@link_resource.source, @link_resource.target)
+        link.create!
+        assert !link.updated?
+      end
     end
-  end
 
-  it 'should set its update status if a link was removed' do
-    FakeFS do
-      link = Wright::Provider::Link.new(@link_resource)
-      FileUtils.ln_sf(@link_resource.source, @link_resource.target)
-      link.remove!
-      assert link.updated?
+    it 'should return the update status if a link was removed' do
+      FakeFS do
+        link = Wright::Provider::Link.new(@link_resource)
+        FileUtils.ln_sf(@link_resource.source, @link_resource.target)
+        link.remove!
+        assert link.updated?
+      end
     end
-  end
 
-  it 'should not change its update status if a link was not removed' do
-    FakeFS do
-      link = Wright::Provider::Link.new(@link_resource)
-      assert !link.updated?
-      link.remove!
-      assert !link.updated?
+    it 'should return the update status if a link was not removed' do
+      FakeFS do
+        link = Wright::Provider::Link.new(@link_resource)
+        link.remove!
+        assert !link.updated?
 
-      FileUtils.touch(@link_resource.target)
-      proc { link.remove! }.must_raise RuntimeError
-      assert !link.updated?
+        FileUtils.touch(@link_resource.target)
+        proc { link.remove! }.must_raise RuntimeError
+        assert !link.updated?
+      end
     end
   end
 end
