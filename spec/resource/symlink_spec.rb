344fd9684b131eb274cb8a3720e9e26ef9ab1cca
Sebastian Boehm <sebastian@sometimesfood.org>
2012-09-17 19:44:25 +0200

    Rename members in symlink resource spec

--- spec/resource/symlink_spec.rb
+++ spec/resource/symlink_spec.rb
@@ -4,15 +4,15 @@ require 'wright/resource/symlink'
 require 'fileutils'
 
 describe Wright::Resource::Symlink do
-  def link_resource(to, name)
-    link = Wright::Resource::Symlink.new(name)
-    link.to = to
+  def link_resource(target, link_name)
+    link = Wright::Resource::Symlink.new(link_name)
+    link.to = target
     link
   end
 
   before(:each) do
-    @to = 'foo'
-    @name = 'bar'
+    @target = 'foo'
+    @link_name = 'bar'
   end
 
   after(:each) do
@@ -21,64 +21,64 @@ describe Wright::Resource::Symlink do
 
   it 'should create symlinks' do
     FakeFS do
-      link = link_resource(@to, @name)
+      link = link_resource(@target, @link_name)
       link.create!
-      assert File.symlink?(@name)
-      File.readlink(@name).must_equal(@to)
+      assert File.symlink?(@link_name)
+      File.readlink(@link_name).must_equal(@target)
     end
   end
 
   it 'should update symlinks to files' do
     FakeFS do
-      FileUtils.ln_sf('oldsource', @name)
-      link = link_resource(@to, @name)
+      FileUtils.ln_sf('oldsource', @link_name)
+      link = link_resource(@target, @link_name)
       link.create!
-      assert File.symlink?(@name)
-      File.readlink(@name).must_equal(@to)
+      assert File.symlink?(@link_name)
+      File.readlink(@link_name).must_equal(@target)
     end
   end
 
   it 'should update symlinks to directories' do
     FakeFS do
       FileUtils.mkdir_p('somedir')
-      FileUtils.ln_s('somedir', @name)
-      link = link_resource(@to, @name)
+      FileUtils.ln_s('somedir', @link_name)
+      link = link_resource(@target, @link_name)
       link.create!
-      assert File.symlink?(@name)
-      File.readlink(@name).must_equal(@to)
+      assert File.symlink?(@link_name)
+      File.readlink(@link_name).must_equal(@target)
     end
   end
 
   it 'should not overwrite existing files' do
     FakeFS do
       file_content = 'Hello world'
-      File.write(@name, file_content)
-      link = link_resource(@to, @name)
+      File.write(@link_name, file_content)
+      link = link_resource(@target, @link_name)
       proc { link.create! }.must_raise(Errno::EEXIST)
-      File.read(@name).must_equal(file_content)
+      File.read(@link_name).must_equal(file_content)
     end
   end
 
   it 'should remove existing symlinks' do
     FakeFS do
-      FileUtils.touch(@to)
-      FileUtils.ln_s(@to, @name)
-      link = Wright::Resource::Symlink.new(@name)
-      assert File.exist?(@to)
-      assert File.symlink?(@name)
+      FileUtils.touch(@target)
+      FileUtils.ln_s(@target, @link_name)
+      link = Wright::Resource::Symlink.new(@link_name)
+      assert File.exist?(@target)
+      assert File.symlink?(@link_name)
       link.remove!
-      assert  File.exist?(@to)
-      assert !File.symlink?(@name)
+      assert  File.exist?(@target)
+      assert !File.symlink?(@link_name)
     end
   end
 
   it 'should not remove existing regular files' do
     FakeFS do
-      FileUtils.touch(@name)
-      link = Wright::Resource::Symlink.new(@name)
-      assert File.exist?(@name)
+      FileUtils.touch(@link_name)
+      link = Wright::Resource::Symlink.new(@link_name)
+      assert File.exist?(@link_name)
       proc { link.remove! }.must_raise RuntimeError
-      assert File.exist?(@name)
+      assert File.exist?(@link_name)
     end
   end
 end
