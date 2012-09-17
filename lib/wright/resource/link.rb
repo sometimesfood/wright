15c56e963cfa155e26c0248ed0a29fe9ac69308d
Sebastian Boehm <sebastian@sometimesfood.org>
2012-09-17 15:45:04 +0200

    Add documentation for link resource

--- lib/wright/resource/link.rb
+++ lib/wright/resource/link.rb
@@ -1,26 +1,48 @@
 require 'wright/resource'
 require 'wright/dsl'
 
+# Public: Link resource, represents a symlink.
+#
+# Examples
+#
+#   link = Wright::Resource::Link.new('/tmp/fstab')
+#   link.to = '/etc/fstab'
+#   link.create!
 class Wright::Resource::Link < Wright::Resource
-  def initialize(resource)
+
+  # Public: Initialize a Link.
+  #
+  # target - The link's target.
+  def initialize(target)
     super
     @source = nil
     @action = :create
   end
-  attr_accessor :source
-  attr_reader   :target
 
+  # Public: Gets/Sets the Link's source.
+  attr_accessor :source
+
+  # Public: Gets the Link's target
+  attr_reader :target
   alias_method :target, :name
-  alias_method :from,   :target
+
+  # Public: Gets/Sets the Link's source.
   alias_method :to,     :source
+  # Public: Gets/Sets the Link's source.
   alias_method :to=,    :source=
 
+  # Public: Create or update the Link.
+  #
+  # Returns nothing.
   def create!
     might_update_resource do
       @provider.create!
     end
   end
 
+  # Public: Remove the Link.
+  #
+  # Returns nothing.
   def remove!
     might_update_resource do
       @provider.remove!
