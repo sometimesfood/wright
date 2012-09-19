b003fa3644135c82a480678968b837bd5cd71944
Sebastian Boehm <sebastian@sometimesfood.org>
2012-09-19 13:49:51 +0200

    Split up symlink resource spec

--- spec/resource/symlink_spec.rb
+++ spec/resource/symlink_spec.rb
@@ -19,66 +19,70 @@ describe Wright::Resource::Symlink do
     FakeFS::FileSystem.clear
   end
 
-  it 'should create symlinks' do
-    FakeFS do
-      link = link_resource(@target, @link_name)
-      link.create!
-      assert File.symlink?(@link_name)
-      File.readlink(@link_name).must_equal(@target)
+  describe '#create!' do
+    it 'should create symlinks' do
+      FakeFS do
+        link = link_resource(@target, @link_name)
+        link.create!
+        assert File.symlink?(@link_name)
+        File.readlink(@link_name).must_equal(@target)
+      end
+    end
+
+    it 'should update symlinks to files' do
+      FakeFS do
+        FileUtils.ln_sf('oldtarget', @link_name)
+        link = link_resource(@target, @link_name)
+        link.create!
+        assert File.symlink?(@link_name)
+        File.readlink(@link_name).must_equal(@target)
+      end
+    end
+
+    it 'should update symlinks to directories' do
+      FakeFS do
+        FileUtils.mkdir_p('somedir')
+        FileUtils.ln_s('somedir', @link_name)
+        link = link_resource(@target, @link_name)
+        link.create!
+        assert File.symlink?(@link_name)
+        File.readlink(@link_name).must_equal(@target)
+      end
+    end
+
+    it 'should not overwrite existing files' do
+      FakeFS do
+        file_content = 'Hello world'
+        File.write(@link_name, file_content)
+        link = link_resource(@target, @link_name)
+        proc { link.create! }.must_raise(Errno::EEXIST)
+        File.read(@link_name).must_equal(file_content)
+      end
     end
   end
 
-  it 'should update symlinks to files' do
-    FakeFS do
-      FileUtils.ln_sf('oldsource', @link_name)
-      link = link_resource(@target, @link_name)
-      link.create!
-      assert File.symlink?(@link_name)
-      File.readlink(@link_name).must_equal(@target)
+  describe '#remove!' do
+    it 'should remove existing symlinks' do
+      FakeFS do
+        FileUtils.touch(@target)
+        FileUtils.ln_s(@target, @link_name)
+        link = Wright::Resource::Symlink.new(@link_name)
+        assert File.exist?(@target)
+        assert File.symlink?(@link_name)
+        link.remove!
+        assert  File.exist?(@target)
+        assert !File.symlink?(@link_name)
+      end
     end
-  end
 
-  it 'should update symlinks to directories' do
-    FakeFS do
-      FileUtils.mkdir_p('somedir')
-      FileUtils.ln_s('somedir', @link_name)
-      link = link_resource(@target, @link_name)
-      link.create!
-      assert File.symlink?(@link_name)
-      File.readlink(@link_name).must_equal(@target)
-    end
-  end
-
-  it 'should not overwrite existing files' do
-    FakeFS do
-      file_content = 'Hello world'
-      File.write(@link_name, file_content)
-      link = link_resource(@target, @link_name)
-      proc { link.create! }.must_raise(Errno::EEXIST)
-      File.read(@link_name).must_equal(file_content)
-    end
-  end
-
-  it 'should remove existing symlinks' do
-    FakeFS do
-      FileUtils.touch(@target)
-      FileUtils.ln_s(@target, @link_name)
-      link = Wright::Resource::Symlink.new(@link_name)
-      assert File.exist?(@target)
-      assert File.symlink?(@link_name)
-      link.remove!
-      assert  File.exist?(@target)
-      assert !File.symlink?(@link_name)
-    end
-  end
-
-  it 'should not remove existing regular files' do
-    FakeFS do
-      FileUtils.touch(@link_name)
-      link = Wright::Resource::Symlink.new(@link_name)
-      assert File.exist?(@link_name)
-      proc { link.remove! }.must_raise RuntimeError
-      assert File.exist?(@link_name)
+    it 'should not remove existing regular files' do
+      FakeFS do
+        FileUtils.touch(@link_name)
+        link = Wright::Resource::Symlink.new(@link_name)
+        assert File.exist?(@link_name)
+        proc { link.remove! }.must_raise RuntimeError
+        assert File.exist?(@link_name)
+      end
     end
   end
 end
