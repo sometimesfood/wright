adbe8c7eccad6defeaed4fb091100a04c1e1e1e6
Sebastian Boehm <sebastian@sometimesfood.org>
2012-10-05 18:25:16 +0200

    Add documentation for dry-run mode

--- lib/wright/dry_run.rb
+++ lib/wright/dry_run.rb
@@ -1,10 +1,28 @@
 module Wright
   @dry_run = false
 
+  # Public: Checks if dry-run mode is currently active.
+  #
+  # Examples
+  #
+  #   puts 'Just a dry-run...' if Wright.dry_run?
+  #
+  # Returns true if dry-run mode is currently active and false otherwise.
   def self.dry_run?
     @dry_run
   end
 
+  # Public: Runs a block in dry-run mode.
+  #
+  # Examples
+  #
+  #   Wright.dry_run do
+  #     symlink '/tmp/fstab' do |s|
+  #       s.to = '/etc/fstab'
+  #     end
+  #   end
+  #
+  # Returns the block's return value.
   def self.dry_run
     saved_dry_run = @dry_run
     @dry_run = true
