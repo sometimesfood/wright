69958121e44070a8c40125947bd2648e8b851317
Andi Fink <finkzeug@gmail.com>
2015-03-03 09:37:24 +0100

    Add --version option

--- lib/wright/cli.rb
+++ lib/wright/cli.rb
@@ -1,8 +1,16 @@
+require 'optparse'
 require 'wright'
 
 module Wright
   class CLI
     def run(argv)
+      @parser = OptionParser.new do |opts|
+        opts.on_tail('-v', '--version', 'Show wright version') do
+          puts "wright version #{Wright::VERSION}"
+          return
+        end
+      end
+      @parser.parse!(argv)
     end
   end
 end
