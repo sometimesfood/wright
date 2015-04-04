76ed471710e537995f2509bb0d4798d762572a95
Sebastian Boehm <sebastian@sometimesfood.org>
2015-04-04 11:58:23 +0200

    Clean up Util::User::next_free_{uid,gid}

--- lib/wright/util/user.rb
+++ lib/wright/util/user.rb
@@ -51,11 +51,7 @@ module Wright
       # @return [Integer] the next free uid
       # @raise [RuntimeError] if there are no free uids in the range
       def self.next_free_uid(uid_range)
-        used_uids = []
-        Etc.passwd { |u| used_uids << u.uid if uid_range.include?(u.uid) }
-        free_uids = uid_range.to_a - used_uids
-        fail "No free uids in uid range #{uid_range}" if free_uids.empty?
-        free_uids.min
+        next_free_id(:uid, uid_range)
       end
 
       # Returns the the next free gid in a range.
@@ -69,12 +65,22 @@ module Wright
       # @return [Integer] the next free gid
       # @raise [RuntimeError] if there are no free gids in the range
       def self.next_free_gid(gid_range)
-        used_gids = []
-        Etc.group { |g| used_gids << g.gid if gid_range.include?(g.gid) }
-        free_gids = gid_range.to_a - used_gids
-        fail "No free gids in gid range #{gid_range}" if free_gids.empty?
-        free_gids.max
+        next_free_id(:gid, gid_range)
       end
+
+      def self.next_free_id(id_type, id_range)
+        fail ArgumentError unless [:uid, :gid].include?(id_type)
+        used_ids = []
+        iterator = id_type == :uid ? Etc.method(:passwd) : Etc.method(:group)
+        iterator.call do |o|
+          id = o.method(id_type).call
+          used_ids << id if id_range.include?(id)
+        end
+        free_ids = id_range.to_a - used_ids
+        fail "No free #{id_type} in range #{id_range}" if free_ids.empty?
+        free_ids.min
+      end
+      private_class_method(:next_free_id)
     end
   end
 end
