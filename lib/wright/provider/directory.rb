e583b695977b4c77d81317c9cfad3360d4ad5a49
Sebastian Boehm <sebastian@sometimesfood.org>
2015-04-15 15:43:13 +0200

    Clean up directory provider update checks

--- lib/wright/provider/directory.rb
+++ lib/wright/provider/directory.rb
@@ -12,14 +12,12 @@ module Wright
       #
       # @return [void]
       def create
-        if ::File.directory?(dirname) && permissions.uptodate?
-          Wright.log.debug "directory already created: '#{@resource.name}'"
-          return
-        end
-
         fail Errno::EEXIST, dirname if regular_file?
-        create_directory
-        @updated = true
+
+        dir = @resource.name
+        unless_uptodate(:create, "directory already created: '#{dir}'") do
+          create_directory
+        end
       end
 
       # Removes the directory.
@@ -30,15 +28,22 @@ module Wright
           fail "'#{dirname}' exists but is not a directory"
         end
 
-        if ::File.directory?(dirname)
+        dir = @resource.name
+        unless_uptodate(:remove, "directory already removed: '#{dir}'") do
           remove_directory
-          @updated = true
-        else
-          Wright.log.debug "directory already removed: '#{@resource.name}'"
         end
       end
 
       private
+
+      def uptodate?(action)
+        case action
+        when :create
+          ::File.directory?(dirname) && permissions.uptodate?
+        when :remove
+          !::File.exist?(dirname) && !::File.directory?(dirname)
+        end
+      end
 
       def permissions
         Wright::Util::FilePermissions.create_from_resource(@resource,
